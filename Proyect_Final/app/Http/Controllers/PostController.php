c710ad4163be6445c8a6b31032e5c6a866890eb7
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-12 16:56:51 -0600

    Mostrando listado de Post y Funcionalidad del boton eliminar

--- Proyect_Final/app/Http/Controllers/PostController.php
+++ Proyect_Final/app/Http/Controllers/PostController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+        return view(
+            'posts.index',
+            ['posts' => Post::latest()->paginate()]
+        );
+    }
+
+    public function destroy(Post $post){
+        $post->delete();
+        return back();
     }
 }
