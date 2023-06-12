9325f9bad66d266800b374a2a01de4f18f8f54f6
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-12 15:38:09 -0600

    Panel administrativo con brazze

---
+++ Proyect_Final/app/Http/Controllers/PageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    public function home()
+    {
+        $title = 'Home';
+        return view('home', compact('title'));
+    }
+
+    public function blog()
+    {
+        $title = 'Blog';
+        // Consulta a BD
+
+        // $posts = Post::get();
+        // $posts = Post::first();
+        // $post = Post::find(25);
+
+        // dd($post);
+
+        // Datos paginados
+        $posts = Post::latest()->paginate();
+        // Codigo quemado
+        // $posts = [
+        //     ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        //     ['id' => 2, 'title' => 'LARAVEL', 'slug' => 'laravel']
+        // ];
+
+        return view('blog', ['posts' => $posts], compact('title'));
+    }
+
+    public function post(Post $post)
+    {
+        $title = 'Blog Personal';
+        // Consulta BD
+        // $post = $slug;
+        return view('post', ['post' => $post], compact('title'));
+    }
+}
