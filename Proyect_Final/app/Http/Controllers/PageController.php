6c74256589e62ef608e997e38ee5eae5af502c6d
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-22 16:39:59 -0600

    Mostranto Contenido de los Post en la vista Home

--- Proyect_Final/app/Http/Controllers/PageController.php
+++ Proyect_Final/app/Http/Controllers/PageController.php
@@ -11,30 +11,32 @@ class PageController extends Controller
     public function home()
     {
         $title = 'Home';
-        return view('home', compact('title'));
-    }
-
-    public function blog()
-    {
-        $title = 'Blog';
-        // Consulta a BD
-
-        // $posts = Post::get();
-        // $posts = Post::first();
-        // $post = Post::find(25);
-
-        // dd($post);
-
-        // Datos paginados
         $posts = Post::latest()->paginate();
-        // Codigo quemado
-        // $posts = [
-        //     ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
-        //     ['id' => 2, 'title' => 'LARAVEL', 'slug' => 'laravel']
-        // ];
 
-        return view('blog', ['posts' => $posts], compact('title'));
+        return view('home', ['posts' => $posts, 'title' => $title]);
     }
+
+    // public function blog()
+    // {
+    //     $title = 'Blog';
+    //     // Consulta a BD
+
+    //     // $posts = Post::get();
+    //     // $posts = Post::first();
+    //     // $post = Post::find(25);
+
+    //     // dd($post);
+
+    //     // Datos paginados
+    //     $posts = Post::latest()->paginate();
+    //     // Codigo quemado
+    //     // $posts = [
+    //     //     ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+    //     //     ['id' => 2, 'title' => 'LARAVEL', 'slug' => 'laravel']
+    //     // ];
+
+    //     return view('blog', ['posts' => $posts], compact('title'));
+    // }
 
     public function post(Post $post)
     {
